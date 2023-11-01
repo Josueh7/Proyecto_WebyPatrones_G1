b87f5590c92b0b8a2fa705b6bffe7667092e4534
Josueh7 <josueh070499@gmail.com>
2023-10-31 18:15:49 -0600

    Arreglo de colores para Login y Registro

--- ProyectoWebYPatronesGrupo1/src/main/java/com/ProyectoWebYPatronesGrupo1/service/impl/MiPerfilServiceImpl.java
+++ ProyectoWebYPatronesGrupo1/src/main/java/com/ProyectoWebYPatronesGrupo1/service/impl/MiPerfilServiceImpl.java
@@ -3,7 +3,6 @@ package com.ProyectoWebYPatronesGrupo1.service.impl;
 import com.ProyectoWebYPatronesGrupo1.dao.MiPerfilDao;
 import com.ProyectoWebYPatronesGrupo1.domain.MiPerfil;
 import com.ProyectoWebYPatronesGrupo1.service.MiPerfilService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +12,6 @@ public class MiPerfilServiceImpl implements MiPerfilService {
 
     @Autowired
     private MiPerfilDao perfilDao;
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<MiPerfil> getInfoPerfil() {
-         List<MiPerfil> informacion = perfilDao.findAll();
-         
-         return informacion;
-    }
 
     @Override
     public MiPerfil getCampoPerfil(MiPerfil perfil) {
