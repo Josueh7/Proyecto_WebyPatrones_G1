477a30a691c89dc43a55f61dbf036b06bef8613b
Danny Enrique Grijalba Gutiérrez <danny.grijal1999@outlook.com>
2023-11-20 00:56:21 -0600

    feat/registro sueño finalizado

--- ProyectoWebYPatronesGrupo1/src/main/java/com/ProyectoWebYPatronesGrupo1/controller/SuenoController.java
+++ ProyectoWebYPatronesGrupo1/src/main/java/com/ProyectoWebYPatronesGrupo1/controller/SuenoController.java
@@ -6,6 +6,7 @@ package com.ProyectoWebYPatronesGrupo1.controller;
 
 import com.ProyectoWebYPatronesGrupo1.domain.Sueno;
 import com.ProyectoWebYPatronesGrupo1.service.SuenoService;
+import static java.rmi.server.LogStream.log;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,8 +42,11 @@ public class SuenoController {
         return "sueno/registrarSueno";
     }
     
-    @PostMapping("/registrar")
-    public String registrarSueno(Sueno sueno, RedirectAttributes redirectAttributes) {
+    
+    
+    @PostMapping("/guardar")
+    public String registrarSueno(Sueno sueno) {
+        
         
         sueno.setFecha(sueno.getMomentoInicio().format(DateTimeFormatter.ISO_DATE));
         
@@ -50,19 +55,36 @@ public class SuenoController {
         long diferenciaHoras = duracion.toHours();
         sueno.setCantHoraSueno((int)diferenciaHoras);
 
-        // Guarda el sueño en la base de datos
         suenoService.save(sueno);
         
-        redirectAttributes.addFlashAttribute("guardadoExitoso", "Registro guardado con éxito");
-        
-        return "redirect:/sueno/regSueno";
+        return "redirect:/sueno/listaSuenos";
     }
     
-     @GetMapping("/listaSuenos")
+    @GetMapping("/listaSuenos")
     public String listaSuenos(Model model) {
         var suenos = suenoService.getInfoRegSuenos();
         model.addAttribute("suenos", suenos);
         return "sueno/listadoSueno";
+    }
+    
+    @GetMapping("/modificarSueno/{idSueno}")
+    public String suenoModificar(@ModelAttribute Sueno sueno, Model model) {
+        sueno = suenoService.getRegSueno(sueno);
+        model.addAttribute("sueno", sueno);
+        return "sueno/modificarSueno";
+    }
+    
+    @GetMapping("/confirmarEliminar/{idSueno}")
+    public String suenoConfirmEliminar(Sueno sueno, Model model){
+        sueno = suenoService.getRegSueno(sueno);
+        model.addAttribute("sueno", sueno);
+        return "/sueno/confirmarEliminarSueno";
+    }
+    
+    @GetMapping("/eliminar/{idSueno}")
+    public String suenoEliminar(Sueno sueno){
+        suenoService.delete(sueno);
+        return "redirect:/sueno/listaSuenos";
     }
 }
 
