3c057a62c6482cef2de287e965dc6e27c027fb99
Josueh7 <josueh070499@gmail.com>
2023-11-21 11:54:34 -0600

    Correcciones Menu Principal y redireccionamiento de vistado sueño

---
+++ ProyectoWebYPatronesGrupo1/src/main/java/com/ProyectoWebYPatronesGrupo1/controller/MenuPrincipalController.java
@@ -0,0 +1,16 @@
+package com.ProyectoWebYPatronesGrupo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MenuPrincipalController {
+    
+    @RequestMapping("/Menu")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "/MenuPrincipal/MenuPrincipal";
+    }
+
+}
